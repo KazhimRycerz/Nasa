2c571ce8a25481b375eae684ccf4e155390623fe
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-06-22 16:19:42 +0200

    finished

--- dist/script.js
+++ dist/script.js
@@ -18,6 +18,7 @@ const displayInfo = (data) => {
         // imgOfTheDay.src = imageUrl
         // imgOfTheDay.style.width="50%"
     const body = document.querySelector("body")
+        body.style.backgroundColor='black'
         body.style.backgroundImage=`url(${imageUrl})`
         body.style.backgroundSize="100vw"
         body.style.backgroundRepeat="no-repeat"
@@ -28,31 +29,34 @@ const displayInfo = (data) => {
         explaination.textContent = data.explanation 
         explaination.style.color="white"
         explaination.style.width="75%"
+        explaination.style.maxWidth="75vw"
         explaination.style.fontSize="1.0rem"
         explaination.style.lineHeight="2.0rem"
         explaination.style.position="relative"
     const date = document.createElement('div')
         date.textContent = data.date
         date.style.color="white"
-        date.style.margin="50px 0 0 50px"
+        date.style.margin="20px 0 0 0px"
     const copyright = document.createElement('p')
-        copyright.textContent = `© ${data.copyright}`
+        copyright.textContent = `©  ${data.copyright}`
+        copyright.style.position="absolute"
+        copyright.style.bottom="20px"
+        copyright.style.left="80px"
+        copyright.style.fontSize="10px"
         copyright.style.color = "white"
+        copyright.style.transform="rotate(-90deg) translateX(20px) translateY(-90px)"
     const container = document.querySelector("#nasaIOTD")
-        container.style.position="relative"
-        container.style.bottom="-40vh"
-        container.style.margin=" 0 3rem 0 3rem "
+        container.style.position="absolute"
+        container.style.bottom="0vh"
+        container.style.margin=" 0 3rem 3rem 3rem "
+        container.style.width="88%"
         container.style.padding="20px"
         container.style.background="rgba(0, 0, 0, 0.5)"
         container.appendChild(imgOfTheDay)
         container.appendChild(title)
         container.appendChild(explaination)
-        body.prepend(date)
+        container.prepend(date)
         body.appendChild(copyright)
-        copyright.style.position="absolute"
-        copyright.style.bottom="20px"
-        copyright.style.left="80px"
-        copyright.style.fontSize="10px"
         
 }
 
